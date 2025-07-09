59254ba7238cbc1fbc8b262a309e2f451d9a3369
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-09 22:46:57 +0000

    feature: add badcredential exception

---
+++ user-service/src/main/java/com/poultry/userservice/exception/BadCredentialException.java
@@ -0,0 +1,7 @@
+package com.poultry.userservice.exception;
+
+public class BadCredentialException extends RuntimeException{
+    public BadCredentialException(String message) {
+        super(message);
+    }
+}
--- user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/poultry/userservice/service/UserServiceImpl.java
@@ -7,7 +7,9 @@ import com.poultry.userservice.entity.User;
 import com.poultry.userservice.mapper.UserMapper;
 import com.poultry.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 @Service
 @RequiredArgsConstructor
@@ -15,16 +17,26 @@ public class UserServiceImpl implements UserService{
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final BCryptPasswordEncoder passwordEncoder;
 
 
     @Override
     public UserResponseDto createUser(UserRequestDto userRequestDto) {
-        User user = userRepository.findByEmail(userRequestDto.email())
-                .orElseThrow(() -> new )
+        userRepository.findByEmail(userRequestDto.email())
+                .orElseThrow(() -> new ResourceAccessException("User with " + userRequestDto.email() + " not found"));
+        User user = userMapper.toEntity(userRequestDto);
+        user.setPassword(passwordEncoder.encode(userRequestDto.password()));
+
+        User savedUser = userRepository.save(user);
+        return userMapper.toDto(savedUser);
     }
 
     @Override
     public UserResponseDto loginUser(UserLoginRequestDto userLoginRequestDto) {
-        return null;
+        User user = userRepository.findByEmail(userLoginRequestDto.email())
+                .orElseThrow(()-> new ResourceAccessException("User with " + userLoginRequestDto.email() + " email not found"));
+
+        if(!passwordEncoder.matches(userLoginRequestDto.password(),user.getPassword()))
+            throw new
     }
 }
