f07192e2179ec14e0073bff5c234acb091d80a3a
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-09 21:47:40 +0000

    feature: add user create user interface

--- user-service/src/main/java/com/poultry/userservice/service/UserService.java
+++ user-service/src/main/java/com/poultry/userservice/service/UserService.java
@@ -4,6 +4,6 @@ import com.poultry.userservice.dto.UserRequestDto;
 import com.poultry.userservice.dto.UserResponseDto;
 
 public interface UserService {
-    UserResponseDto login(UserRequestDto userRequestDto);
+    UserResponseDto createUser(UserRequestDto userRequestDto);
 
 }
