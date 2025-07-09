5e9cf55f3eee16db42ecbf913957818f9f61f356
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-09 12:18:39 +0000

    feature: add security config for http routes

--- authservice/src/main/java/com/poultry/authservice/config/SecurityConfig.java
+++ authservice/src/main/java/com/poultry/authservice/config/SecurityConfig.java
@@ -2,13 +2,25 @@ package com.poultry.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return  http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/auth/v1").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .build();
 
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
