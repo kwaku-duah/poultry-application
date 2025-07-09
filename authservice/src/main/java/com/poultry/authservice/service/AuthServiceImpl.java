01dd5beb78eda8da71bfd1111c6c9f17ff299afb
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-09 11:51:05 +0000

    feature: add user service client to auth service implementation

---
+++ authservice/src/main/java/com/poultry/authservice/service/AuthServiceImpl.java
@@ -0,0 +1,22 @@
+package com.poultry.authservice.service;
+
+
+import com.poultry.authservice.client.UserServiceClient;
+import com.poultry.authservice.dto.AuthRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    private final UserServiceClient userServiceClient;
+
+    public AuthServiceImpl(UserServiceClient userServiceClient) {
+        this.userServiceClient = userServiceClient;
+    }
+
+    @Override
+    public Mono<String> login(AuthRequestDto authRequest) {
+        return null;
+    }
+}
