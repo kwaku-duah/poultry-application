dc59eaa9a61df4655c9538fb847fde7ea8a78ec2
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-09 10:05:59 +0000

    chore: add project setup for auth service

--- authservice/src/main/java/com/poultry/authservice/AuthserviceApplication.java
+++ authservice/src/main/java/com/poultry/authservice/AuthserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+public class AuthserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AuthserviceApplication.class, args);
+    }
 
 }
