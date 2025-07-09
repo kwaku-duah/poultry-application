bc096dcc2d53a7e4f7d9da80cb31bcfa5816c558
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-09 11:22:31 +0000

    feature: add web client injection to communicate with the user -service

---
+++ authservice/src/main/java/com/poultry/authservice/client/UserServiceClient.java
@@ -0,0 +1,16 @@
+package com.poultry.authservice.client;
+
+import com.poultry.authservice.config.WebClientConfig;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class UserServiceClient {
+    private final WebClient webClient;
+
+
+
+    public UserServiceClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+}
