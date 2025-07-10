f3b47d053b8bf6643612d84e61db71ef80f615f8
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-10 11:25:10 +0000

    feature: add reactive user repository

---
+++ user-service/src/main/java/com/poultry/userservice/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.poultry.userservice.repository;
+
+import com.poultry.userservice.entity.User;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveCrudRepository<User, Long> {
+    Mono<User> findByEmail(String email);
+}
