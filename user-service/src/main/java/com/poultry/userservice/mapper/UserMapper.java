f727c8f7ea7e4f90d4feb7b04c7df58d0860f941
Sheikhman_Kalderon <duah229@outlook.com>
2025-07-10 11:48:26 +0000

    feature: add user mapper for worldclass coding

--- user-service/src/main/java/com/poultry/userservice/mapper/UserMapper.java
+++ user-service/src/main/java/com/poultry/userservice/mapper/UserMapper.java
@@ -4,11 +4,13 @@ import com.poultry.userservice.dto.UserRequestDto;
 import com.poultry.userservice.dto.UserResponseDto;
 import com.poultry.userservice.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
 //    User toEntity(UserRequestDto userRequestDto);
 
+    @Mapping(target = "fullName", ignore = true)
     UserResponseDto toDto(User user);
 }
